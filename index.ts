a0ef2ac7770acd2a67b796ab93f24c3642adb105
Xevion <xevion@xevion.dev>
2023-08-11 02:28:21 -0500

    Basic non-typescript demo script, .gitignore logs

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
 import * as cheerio from "cheerio";
+import * as fs from "fs";
+
+const $ = cheerio.load(fs.readFileSync('list.html'));
+const classes = $('#scheduleListView').children('.listViewWrapper').toArray();
+
+const getClassName = (html) => {
+    console.log(html);
+    return html.find('list-view-course-title').text();
+}
+
+console.log(`${classes.length} classes identified.`);
+console.log(`First class: ${getClassName(classes[0])}`);
